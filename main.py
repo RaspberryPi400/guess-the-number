f3ed6af79257ff368a4c541fc5109b4b9de39b67
Elijah Corwin <raspberrypi500@yahoo.com>
2026-02-16 13:54:44 -0500

    Made various changes
    Made many changes to the program, including features like asking if you want to go again or not and it displays your high score after each round. Also made adjustments to the questions that it asks.

--- main.py
+++ main.py
@@ -1,33 +1,48 @@
 import random
+import sys
 
-attemps = 0
-number_list = list(range(1, 31))
-number = random.choice(number_list)
+high_score = 1000000
+
 running = True
 
 while running:
-    guess = float(input("Guess a whole number: "))
-    if guess == number:
-        attemps = attemps + 1
-        running = False
-    elif guess > number:
-        print(f"The number is less than ", guess)
-        attemps = attemps + 1
-    elif guess < number:
-        print(f"The number is greater than ", guess)
-        attemps = attemps + 1
+    attemps = 0
+    number_list = list(range(1, 31))
+    number = random.choice(number_list)
+    guessing = True
+    while guessing:
+        guess = int(input("Guess a whole number between 1 and 30: "))
+        if guess == number:
+            attemps = attemps + 1
+            print("You guessed the number!")
+            if attemps > 1:
+                print(f"It took you ", attemps, " trys.")
+            elif attemps == 1:
+                print("It took you 1 try!")
+            print("""
+            Here is a scoring chart! See how you scored:
+            1 guess: Artificially intelligent
+            2-5 guesses: Amazing
+            6-10 guesses: Great
+            11-15 guesses: Okay
+            16-23 guesses: Bad
+            24+ guesses: Awful
+                """)
+            
+            if attemps < high_score:
+                high_score = attemps
+    
+            print(f"Your high score is ", high_score)
+            replay = input("Would you like to play again? Type 'yes'/'no': ")
+            if replay == "yes":
+                break
+            elif replay == "no":
+                print("Exiting...")
+                sys.exit(0)
 
-print("You guessed the number!")
-if attemps > 1:
-    print(f"It took you ", attemps, " trys.")
-elif attemps == 1:
-    print("It took you 1 try!")
-print("""
-Here is a scoring chart! See how you scored:
-1 guess: Artificially intelligent
-2-5 guesses: Amazing
-6-10 guesses: Great
-11-15 guesses: Okay
-16-23 guesses: Bad
-24+ guesses: Awful
-      """)
+        elif guess > number:
+            print(f"The number is less than ", guess)
+            attemps = attemps + 1
+        elif guess < number:
+            print(f"The number is greater than ", guess)
+            attemps = attemps + 1
